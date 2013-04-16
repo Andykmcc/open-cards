2ec78346eace4777788a583a29573581ac881a94
Andy McCoy <andy.k.mccoy@gmail.com>
2013-04-16 09:07:32 -0700

    resize event added but not working yet

--- js/vendor/jquery.cards.js
+++ js/vendor/jquery.cards.js
@@ -1,20 +1,43 @@
 (function( $ ){
+  var Cards = [],
+      activate = null,
+      resizeHappened  = false,
+      newscreensize = 0;
 
   methods = {
     init : function( options ) {
       var docwidth = document.width || window.width - 15,
-          activate = options ? options.activate : 'mouseenter mouseleave';
+          activate = options ? options.activate : 'mouseenter mouseleave',
+          Cards = this;
 
-      return this.each(function(i, el){
-        var $el = $(el),
+      setTimeout(function(){
+        if(resizeHappened){
+          resizeHappened = false;
+
+          return Cards.each(function(i, el){
+            methods.setup(i, el, newscreensize, activate);
+          });
+        }
+        setTimeout(arguments.callee, 10);
+      }, 10);
+
+      $(window).resize(function(){ resizeHappened = true; newscreensize = this.document.width; });
+
+      return Cards.each(function(i, el){
+        methods.setup(i, el, docwidth, activate);
+      });
+
+    },
+    setup : function(i, el, documentwidth, activateevent){
+      var $el = $(el),
             elWidth = $el.width(),
             elLeft = $el.position().left,
-            isRight = elLeft + (2 * elWidth) < docwidth ? 1 : 0,
+            isRight = elLeft + (2 * elWidth) < documentwidth ? 1 : 0,
             opendirection = isRight ? 'cards-openRight' : 'cards-openLeft',
             layers = $el.children('div');
 
         
-        $el.addClass('cards-container '+opendirection).on( activate, methods.toggle ).css({'z-index': ++i*100 }).data('org-zindex', ++i*100);
+        $el.addClass('cards-container '+opendirection).on( activateevent, methods.toggle ).css({'z-index': ++i*100 }).data('org-zindex', ++i*100);
 
         layers.each(function(i, layer){
           if( i === 0){
@@ -27,7 +50,6 @@
             $(layer).addClass('cards-layer cards-animated cards-back');
           }
         });
-      });
     },
     toggle : function(){
       var $this = $(this),
@@ -41,6 +63,13 @@
         $this.addClass('cards-open').css( { 'z-index' : zindex*100 } );
         $this.trigger('cards-open');
       }
+    },
+    resizeAdjust : function( newWidth ){
+
+      return Cards.each(function(i, el){
+        methods.setup(i, el, newWidth, activate);
+      });
+
     }
   };
 
